0b87734bcb391b969245d98e0c59432760e32594
ipChrisLee <ipChrisLee@outlook.com>
2024-09-27 17:42:40 +0800

    feat: tmp

--- cf.py
+++ cf.py
@@ -4,6 +4,8 @@
 from pathlib import Path
 import termcolor
 import argparse
+from cf_str_literal import bist_stdcpp_h_str, default_cpp_file_str, default_py_file_str
+import os
 
 ap = argparse.ArgumentParser("cf")
 ap_subparsers = ap.add_subparsers(dest="func", required=True)
@@ -30,6 +32,10 @@ def main_init_folder(init_folder_path: str):
     init_folder_path.mkdir(exist_ok=True)
     (init_folder_path / "tests").mkdir(exist_ok=True)
     (init_folder_path / "bin").mkdir(exist_ok=True)
+    include_bits_folder_path = (init_folder_path / "include" / "bits")
+    os.makedirs(include_bits_folder_path)
+    with open((include_bits_folder_path / "stdc++.h"), "w") as fp:
+        fp.write(bist_stdcpp_h_str)
     with open((init_folder_path / ".anchor"), "w") as fp:
         fp.write(anchor_str)
         fp.write("\n")
@@ -42,7 +48,13 @@ def main_start(start_q: str, lang: str):
         raise FileExistsError(
             f"Creating existing file {p}, remove it and start then.")
     with open(p, "w") as fp:
-        pass
+        if lang == "cpp":
+            fp.write(default_cpp_file_str)
+        elif lang == "py":
+            fp.write(default_py_file_str)
+        else:
+            raise ValueError(f"Unsupported to start with lang {lang}")
+    # TODO: append info to compile_commands?
 
 
 if __name__ == '__main__':
--- demo/run-demo.sh
+++ demo/run-demo.sh
@@ -2,8 +2,10 @@
 
 if ! (which cf.py > /dev/null 2>&1); then
 	export PATH="$PATH:$(cd ..; pwd)"
+	export PYTHONPATH="$PATH:$(cd ../lib; pwd)"
 fi
 
+rm -rf contest
 cf.py init_folder contest
 pushd contest
 cf.py start A
