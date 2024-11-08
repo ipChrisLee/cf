b4f93a2e1805394438bc8e853b4f54f16c3b5f73
liyuanshuo.chris <liyuanshuo.chris@bytedance.com>
2024-11-08 15:48:42 +0800

    refactor(tempalte): format euler-sieve

--- templates/euler-sieve.cpp
+++ templates/euler-sieve.cpp
@@ -3,27 +3,24 @@
 
 using i32 = int32_t;
 
-
-// ========
+// ========================
 struct EulerSieve {
 	i32 endNum_; // exclusive
 	std::vector<bool> isPrime_;
 	std::vector<i32> mnFactor_;
 	std::vector<i32> primes_;
-	EulerSieve(i32 endNum) : endNum_(endNum), isPrime_(endNum_, true), mnFactor_(endNum_, 1){
-		for (i32 i=2; i<endNum_; ++i) {
+	EulerSieve(i32 endNum) : endNum_(endNum), isPrime_(endNum_, true), mnFactor_(endNum_, 1) {
+		for (i32 i = 2; i < endNum_; ++i) {
 			if (isPrime_[i]) {
 				primes_.emplace_back(i);
-			} 
-			for (i32 j : primes_) {
+			}
+			for (i32 j: primes_) {
 				if (i * j >= endNum_) { break; }
-				if (isPrime_[i*j]) {
-					mnFactor_[i*j] = std::min(i, j);
-					isPrime_[i*j] = false;
+				if (isPrime_[i * j]) {
+					mnFactor_[i * j] = std::min(i, j);
+					isPrime_[i * j] = false;
 				}
-				if (i % j == 0) {
-					break;
-				}
+				if (i % j == 0) { break; }
 			}
 		}
 	}
@@ -31,7 +28,7 @@ struct EulerSieve {
 	i32 min_factor(i32 x) { return mnFactor_[x]; }
 	i32 ith_prime(i32 i) { return primes_[i]; }
 };
-// ========
+// ========================
 
 int main() {
 	return 0;
